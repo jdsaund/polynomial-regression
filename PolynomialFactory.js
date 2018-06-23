2f09582a2da47316fa3303aa1afacce973dba60b
Jolyon Saunders <jol@formalytics.io>
2018-06-23 14:10:41 +0800

    training polynomial creation

--- PolynomialFactory.js
+++ PolynomialFactory.js
@@ -3,18 +3,42 @@
 const tf = require('@tensorflow/tfjs')
 const Polynomial = require('./Polynomial')
 
+// A normally distributed coeffecient
+const randomCoefficient = (sigma) => tf.randomNormal([1], 0, sigma)
+
+// A normally distributed variable coefficient.
+const initialTrainingCoefficient = (sigma) => tf.variable(randomCoefficient(sigma))
+
+// This function returns a Polynomial with coefficeints defined by an initialiser function.
+const polynomialFromInitialiser = (degree, initialiser) => {
+  const coefficients = Array.apply(null, Array(degree + 1))
+    .map(_ => initialiser)
+
+  return new Polynomial(coefficients)
+}
+
 class PolynomialFactory {
   /**
-   * @static randomPolynomial - Creates a Polynomial with normally distributed coefficients.
+   * @static randomPolynomial - Creates a random Polynomial with normally distributed coefficients.
    *
    * @param  {int} degree The degree of the Polynomial.
-   * @return {Polynomial} The Plynomial.
+   * @param  {number} sigma The standard deviation of the coefficients.
+   * @return {Polynomial} The Polynomial.
    */
   static randomPolynomial (degree, sigma = 0.04) {
-    const coefficients = Array.apply(null, Array(degree + 1))
-      .map(_ => tf.randomNormal([1], 0, sigma))
+    return polynomialFromInitialiser(degree, randomCoefficient(sigma))
+  }
 
-    return new Polynomial(coefficients)
+  /**
+   * @static trainingPolynomial - Creates a Polynomial for training,
+   * initialised with normally distributed coefficients.
+   *
+   * @param  {int} degree The degree of the Polynomial.
+   * @param  {number} sigma The standard deviation of the coefficients.
+   * @return {Polynomial} The Polynomial.
+   */
+  static trainingPolynomial (degree, sigma = 1.0) {
+    return polynomialFromInitialiser(degree, initialTrainingCoefficient(sigma))
   }
 }
 
