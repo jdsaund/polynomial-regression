c7eae9434c695793c2c2767bf559e42de7d9c8d9
Jolyon Saunders <jol@formalytics.io>
2018-06-23 22:57:36 +1000

    added materialize css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Header</h1>
-        </header>
-        <p className='App-intro'>
+        <div className='header black white-text'>
+          Header text
+        </div>
+        <p className='darken-4'>
           Body text
         </p>
       </div>
