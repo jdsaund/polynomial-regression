c380540b5c9aaaa092d8adacfd3c922f839943f9
Jolyon Saunders <jol@formalytics.io>
2018-06-23 21:30:22 +1000

    initialised react app and merged with current project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Header</h1>
+        </header>
+        <p className='App-intro'>
+          Body text
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
