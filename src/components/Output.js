89f9fe5269e43c2b551348c11a67d3e1a5d2f4fe
Jolyon Saunders <jol@formalytics.io>
2018-06-25 00:53:52 +1000

    tidy up of output conatiner

--- src/components/Output.js
+++ src/components/Output.js
@@ -17,46 +17,16 @@ class Output extends Component {
   }
 
   componentDidMount () {
-    this.updateTrainingData()
-      .then(() => {
-        return this.setState({
-          datasets: [this.trainingChartData]
-        })
-      })
-  }
-
-  updateTrainingData () {
-    return DataRasterizer.tensorDataToChartData(this.props.trainingData.xs, this.props.trainingData.ys)
-      .then(data => {
-        this.trainingChartData = ChartData('Training data', data)
-      })
+    this.updateTrainingInput()
   }
 
   componentDidUpdate (prevProps) {
     if (prevProps.trainingData !== this.props.trainingData) {
-      this.updateTrainingData()
-        .then(() => {
-          return this.setState({
-            datasets: [this.trainingChartData]
-          })
-        })
+      this.updateTrainingInput()
     }
 
     if (prevProps.fitted !== this.props.fitted) {
       this.updateTrainingOutput(this.props.fitted)
-    }
-  }
-
-  updateTrainingOutput (polynomial) {
-    if (polynomial) {
-      return DataRasterizer.rasterizePolynomial(polynomial)
-        .then(data => {
-          this.fittedChartData = ChartData('Fitted curve', data, 'red', true)
-          const datasets = [this.fittedChartData, this.trainingChartData]
-          return this.setState({
-            datasets: datasets
-          })
-        })
     }
   }
 
@@ -71,6 +41,39 @@ class Output extends Component {
         </Row>
       </div>
     )
+  }
+
+  /**
+   * updateTrainingInput - Updates the training input data for charting.
+   *
+   * @return {Promise} Promise to update the state.
+   */
+  updateTrainingInput () {
+    return DataRasterizer.tensorDataToChartData(this.props.trainingData.xs, this.props.trainingData.ys)
+      .then((data) => {
+        this.trainingChartData = ChartData('Training data', data)
+        return this.setState({
+          datasets: [this.trainingChartData]
+        })
+      })
+  }
+
+  /**
+   * updateTrainingInput - Updates the training output data for charting.
+   *
+   * @return {Promise} Promise to update the state.
+   */
+  updateTrainingOutput (polynomial) {
+    if (polynomial) {
+      return DataRasterizer.rasterizePolynomial(polynomial)
+        .then(data => {
+          this.fittedChartData = ChartData('Fitted curve', data, 'red', true)
+          const datasets = [this.trainingChartData, this.fittedChartData]
+          return this.setState({
+            datasets: datasets
+          })
+        })
+    }
   }
 }
 
