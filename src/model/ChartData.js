00536222f0b0ca807fb9b06c1344c2e7a9fd8398
Jolyon Saunders <jol@formalytics.io>
2018-06-24 19:17:55 +1000

    added chart data model

---
+++ src/model/ChartData.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const ChartData = (label, data, colour = 'teal', showLine = true) => {
+  return {
+    label: label,
+    fill: false,
+    showLine: false,
+    backgroundColor: `${colour} lighten-3`,
+    pointBorderColor: colour,
+    pointBackgroundColor: '#fff',
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: colour,
+    pointHoverBorderColor: `${colour} darken-3`,
+    pointHoverBorderWidth: 2,
+    pointRadius: 3,
+    pointHitRadius: 10,
+    data: data
+  }
+}
+
+export default ChartData
