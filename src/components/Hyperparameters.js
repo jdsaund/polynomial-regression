cc8d1fa298e2588c2ae60b8ba3883354cc1bf890
Jolyon Saunders <jol@formalytics.io>
2018-06-24 11:43:52 +1000

    added event handling for hyperparameters

--- src/components/Hyperparameters.js
+++ src/components/Hyperparameters.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react'
-import {Row, Col, Input} from 'react-materialize'
+import {Row, Col} from 'react-materialize'
+import InputSelect from './InputSelect'
 
 class Hyperparameters extends Component {
+  updateOptimiser (event) {
+    console.log(`optimiser changed to ${event.target.value}`)
+  }
+
+  updateLossFunction (event) {
+    console.log(`loss function changed to ${event.target.value}`)
+  }
+
+  updateLearningRate (event) {
+    console.log(`learning rate changed to ${event.target.value}`)
+  }
+
+  updateDegree (event) {
+    console.log(`degree changed to ${event.target.value}`)
+  }
+
   render () {
+    const degreeOpts = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+
+    const learningRateOpts = [
+      '0.05',
+      '0.10',
+      '0.25',
+      '0.50',
+      '0.75',
+      '1.00'
+    ]
+
+    const optimiserOpts = [
+      'SGD',
+      '9',
+      '8',
+      '7'
+    ]
+
+    const lossFunctionOpts = [
+      'Mean Squared Error',
+      '2',
+      '3',
+      '4'
+    ]
+
     return (
       <Row className='z-depth-2 white black-text offset-s2 section'>
         <Row>
@@ -12,27 +61,18 @@ class Hyperparameters extends Component {
         </Row>
         <Row>
           <Col offset='s2' s={12}>
-            <Input s={2} placeholder='Placeholder' label='Degree' defaultValue='3' />
-            <Input s={2} type='select' label='Learning rate' defaultValue='0.25'>
-              <option value='0.01'>0.05</option>
-              <option value='0.1'>0.1</option>
-              <option value='0.25'>0.25</option>
-              <option value='0.5'>0.5</option>
-              <option value='0.75'>0.75</option>
-              <option value='1.0'>0.75</option>
-            </Input>
-            <Input s={2} type='select' label='Optimiser' defaultValue='0'>
-              <option value='0'>Option 0</option>
-              <option value='1'>Option 1</option>
-              <option value='2'>Option 2</option>
-              <option value='3'>Option 3</option>
-            </Input>
-            <Input s={2} type='select' label='Loss function' defaultValue='0'>
-              <option value='0'>Option 0</option>
-              <option value='1'>Option 1</option>
-              <option value='2'>Option 2</option>
-              <option value='3'>Option 3</option>
-            </Input>
+            <Col s={2}>
+              <InputSelect optionValues={degreeOpts} label='Degree' onChange={this.updateDegree.bind(this)} />
+            </Col>
+            <Col s={2}>
+              <InputSelect optionValues={learningRateOpts} label='Learning rate' onChange={this.updateLearningRate.bind(this)} />
+            </Col>
+            <Col s={2}>
+              <InputSelect optionValues={optimiserOpts} label='Optimiser' onChange={this.updateOptimiser.bind(this)} />
+            </Col>
+            <Col s={2}>
+              <InputSelect optionValues={lossFunctionOpts} label='Loss function' onChange={this.updateLossFunction.bind(this)} />
+            </Col>
           </Col>
         </Row>
       </Row>
---
+++ src/components/InputSelect.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {Input} from 'react-materialize'
+
+class InputSelect extends Component {
+  render () {
+    let options = this.props.optionValues.map(value => {
+      return <option key={value} value={value}>{value}</option>
+    })
+
+    return (
+      <Input type='select' label={this.props.label || 'Label'} defaultValue={this.props.defaultValue || this.props.optionValues[0]} onChange={this.props.onChange}>
+        {options}
+      </Input>
+    )
+  }
+}
+
+export default InputSelect
